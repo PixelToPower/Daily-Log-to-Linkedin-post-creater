4753fceeb5eae972aced0e592ca4e1aa81570b20
Akshat Panchal <AkshatPanchal0001@outlook.com>
2026-07-18 23:27:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+document.getElementById("savebtnn").addEventListener("click", (savebtnn) => {
+    console.log(savebtnn, "Clicked savebtnn");
+
+    let daily_log_save = document.getElementById("daily-log").value;
+    console.log(daily_log_save)
+
+    let all_logs = []
+    console.log(all_logs)
+})
+
+
+    let save_past_days_log = document.getElementById("logs-store").value;
+    console.log(save_past_days_log)
+
+
+
+document.getElementById("selectbtn").addEventListener("click", (selectbtn) => {
+    console.log(selectbtn, "clicked selectbtn")
+
+    let select_past_log = document.getElementById("logs-store").value;
+    console.log(select_past_log)
+})
+
+
+
+document.getElementById("copybtn").addEventListener("click", (copybtn) => {
+    console.log(copybtn, "clicked copybtn")
+
+    let copy_generated_past_log = document.getElementById("generatedpost").value;
+    console.log(copy_generated_past_log)
+})
+
+
+
+
+document.getElementById("generatebtn").addEventListener("click", (generatebtn) => {
+    console.log(generatebtn, "clicked generatebtn")
+
+})
+
+
+
+
+
